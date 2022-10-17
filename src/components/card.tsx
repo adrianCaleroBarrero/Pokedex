d3f7e09dcabff578a530395394a46346c1f5b108
Adrian <acalerobarrero@gmail.com>
2022-10-17 20:57:43 +0200

    Add image in card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,16 +1,22 @@
-import { iOnePokemon } from "../interfaces/interfaces";
+import { Link } from 'react-router-dom';
+import { iOnePokemon } from '../interfaces/interfaces';
 
 export function Card({ pokemon }: { pokemon: iOnePokemon }) {
-  return (
-    <li>
-      <img
-        src={
-          pokemon.sprites.versions["generation-v"]["black-white"].animated
-            .front_default
-        }
-        alt={pokemon.name}
-      />
-      <p>{pokemon.name}</p>
-    </li>
+  return pokemon.sprites.front_default ? (
+    <Link to={`detail/${pokemon.id}`} state={pokemon}>
+      <li>
+        <img
+          src={
+            pokemon.sprites.other['official-artwork'].front_default
+              ? pokemon.sprites.other['official-artwork'].front_default
+              : pokemon.sprites.front_default
+          }
+          alt={pokemon.name}
+        />
+        <p>{pokemon.name}</p>
+      </li>
+    </Link>
+  ) : (
+    <></>
   );
 }
